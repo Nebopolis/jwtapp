c36e267e18d4bb48929d42aa6e31222d8cf11ed6
Ben Evans <bevans@zendesk.com>
2014-08-04 17:08:05 -0500

    Got JWT to encode properly
    still getting a signing error, but the JWT now decodes at a jwt decoder

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ var md5 = require('md5.js');
   },
 
   events: {
-      'app.activated':'doSomething'
+      'app.activated':'init'
     },
 
-    doSomething: function() {
-      var test = Crypt.encrypt("bah", "somethingtoencrypt", {count:2048, salt:"salt", ks:256});
-      var header = { typ: 'JWT', alg: 'HS256'};
+    init: function() {
       var d = new Date();
       var payload = {
         "jti": md5.MD5("poop").toString(),
@@ -22,20 +20,34 @@ var md5 = require('md5.js');
         "email": "dpawluk@zendesk.com"
 
       };
-      var segments = [];
-      var secret = 'somelongsecretforauthingJWT';
-      var hash = Crypt.hash.sha256;
-      var key = Crypt.codec.utf8String.toBits(secret);
-      var hmac = new Crypt.misc.hmac(key, hash);
-      var result = hmac.encrypt(payload);
-      var end = this.base64urlEncode(b64.decode(Crypt.codec.base64.fromBits(result)));
-      segments.push(this.base64urlEncode(JSON.stringify(header)));
-      segments.push(this.base64urlEncode(JSON.stringify(payload)));
-      console.log(end);
+      var secret = 'the secret';
+
+      var payload = this.JWTEncode(secret,payload);
+      console.log(payload);
+      var url = this.JWTURLCreate('subdomain',payload,'https://subdomain.zendesk.com/agent')
+      console.log(url);
     },
 
-    base64urlEncode: function (arg)
-  {
+
+  JWTEncode: function(key,payload) {
+      var segments = [];
+      var header = { typ: 'JWT', alg: 'HS256'};
+
+      var key = Crypt.codec.utf8String.toBits(key);
+      var hmac = new Crypt.misc.hmac(key);
+
+      segments.push(this.base64urlEncode(JSON.stringify(header)));
+      segments.push(this.base64urlEncode(JSON.stringify(payload)));
+      segments.push(this.base64urlEncode(b64.decode(Crypt.codec.base64.fromBits(hmac.encrypt(segments.join('.'))))));
+
+      return(segments.join('.'));
+  },
+
+  JWTURLCreate: function(subdomain, payload, redirect) {
+    return helpers.fmt("https://%@.zendesk.com/access/jwt?jwt=%@&return_to=%@", subdomain, payload, redirect)
+  },
+
+  base64urlEncode: function (arg) {
     var s = b64.encode(arg); // Standard base64 encoder
     s = s.split('=')[0]; // Remove any trailing '='s
     s = s.replace(/\+/g, '-'); // 62nd char of encoding
@@ -43,6 +55,6 @@ var md5 = require('md5.js');
     // TODO optimize this; we can do much better
     return s;
   }
-  };
 
+};
 }());
